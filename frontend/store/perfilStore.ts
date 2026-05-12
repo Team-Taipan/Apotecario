afbae7d273b21c601650a94a971cd363aff18bdc
dotToras <odamasenganumeko@gmail.com>
2026-05-12 09:43:29 -0300

    feat(perfilStore): adicionado perfilStore e dependencia Zustand

---
+++ frontend/store/perfilStore.ts
@@ -0,0 +1,25 @@
+
+import { create } from "zustand";
+import { ImageSourcePropType } from "react-native";
+
+type Avatar = { id: string; res: ImageSourcePropType; name: string };
+
+const AVATARES: Avatar[] = [
+    { id: '1', res: require("@assets/avatars/brekibedi.png"), name: 'Otavio' },
+    { id: '2', res: require("@assets/avatars/afro.png"), name: 'Mariana' },
+    { id: '3', res: require("@assets/avatars/dorock.png"), name: 'Ricardo' },
+    { id: '4', res: require("@assets/avatars/habibi.png"), name: 'Carla' },
+];
+
+interface PerfilStore {
+    AVATARES: Avatar[];
+    avatarSelecionado: Avatar;
+    setAvatarSelecionado: (avatar: Avatar) => void;
+}
+
+export const usePerfilStore = create<PerfilStore>(() => ({
+    AVATARES,
+    avatarSelecionado: AVATARES[0],
+    setAvatarSelecionado: (avatar) =>
+        usePerfilStore.setState({ avatarSelecionado: avatar }),
+}));
